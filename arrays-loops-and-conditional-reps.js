a54bccfdeccae442e278cc6658c81044f1c9a87f
ArnoldPires <arnold.pires92@gmail.com>
2023-07-27 15:34:26 -0400

    Get Even answered

--- arrays-loops-and-conditional-reps.js
+++ arrays-loops-and-conditional-reps.js
@@ -25,6 +25,17 @@ for (let i = 1; i <= 20; i++) {
 }
 
 ////////////////////////////////
+// Get Even
+////////////////////////////////
+
+//Write a for loop that will log only the even numbers in 0 through 200.
+for (let i = 0; i <= 200; i++ ) {
+  if (i % 2 === 0) {
+    console.log(i)
+  }
+}
+
+////////////////////////////////
 // Fizz Buzz
 ////////////////////////////////
 
