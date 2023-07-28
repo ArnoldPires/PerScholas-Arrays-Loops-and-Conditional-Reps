5f4a29a2bf30bb2b44824171a1f8c66d90c83100
ArnoldPires <arnold.pires92@gmail.com>
2023-07-27 20:57:54 -0400

    Find the Median answered

--- arrays-loops-and-conditional-reps.js
+++ arrays-loops-and-conditional-reps.js
@@ -178,9 +178,23 @@ for (let i = 1; i <= 20; i++) {
 // Find the Median
 ////////////////////////////////
 
-// Find the median number in the following numsarray, then console.log that number.
-
+// Find the median number in the following nums array, then console.log that number.
 //hint if you check the length of the array / 2, you might get not get a whole number. In which case, look into Math.floor( // something )
+/*
+const nums = [14,11,16,15,13,16,15,17,19,11,12,14,19,11,15,17,11,18,12,17,12,71,18,15,12];
+
+Expected output:
+=> 15
+*/
+const nums = [14,11,16,15,13,16,15,17,19,11,12,14,19,11,15,17,11,18,12,17,12,71,18,15,12];
+nums.sort((a, b) => a - b);
+const midIndex = Math.floor(nums.length / 2);
+if (nums.length % 2 === 1) {
+  get15 = nums[midIndex];
+} else {
+  get15 = (nums[midIndex - 1] + nums[midIndex]) / 2;
+}
+console.log(get15);
 
 //Hungry for More?
 
